bc76dce4779a13dec4d02d5c380b43c1b78f41a2
Naninara <106659791+Naninara@users.noreply.github.com>
2022-09-11 20:09:49 +0530

    import java.util.*; class pattern35java {     public static void main(String[] args) {     Scanner sc= new Scanner(System.in);     String s;     s = sc.nextLine();     System.out.print(s);     }  }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class pattern35java {
+    public static void main(String[] args) {
+    Scanner sc= new Scanner(System.in);
+    String s;
+    s = sc.nextLine();
+    System.out.print(s);
+    }
+ }   
