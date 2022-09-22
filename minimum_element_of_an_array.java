599e68110c33be2a9f0e1d83013a077e885e3130
Naninara <106659791+Naninara@users.noreply.github.com>
2022-09-22 14:45:44 +0530

    import java.util.*; class sample{     public static void main(String[] args) {         Scanner sc = new Scanner(System.in);         int n,x[];         n = sc.nextInt();         x = new int[n];         for(int i=0;i<n;i++){             x[i]= sc.nextInt();         }         int min=x[0];         for(int i=1;i<n;i++){             if(x[i]<min){                 min = x[i];             }         }         System.out.println(min);     } }

---
+++ minimum_element_of_an_array.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class sample{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n,x[];
+        n = sc.nextInt();
+        x = new int[n];
+        for(int i=0;i<n;i++){
+            x[i]= sc.nextInt();
+        }
+        int min=x[0];
+        for(int i=1;i<n;i++){
+            if(x[i]<min){
+                min = x[i];
+            }
+        }
+        System.out.println(min);
+    }
+}
