cc551b4e9a91e7bdc7cdc4943b6de3ad34f98470
Naninara <106659791+Naninara@users.noreply.github.com>
2022-09-26 21:00:12 +0530

    import java.util.*; class sample{     public static void main(String[] args) {         Scanner sc = new Scanner(System.in);         int n,x[],c=0;         n = sc.nextInt();         x = new int[n];         for(int i=0;i<n;i++){             x[i]= sc.nextInt();         }         for(int i=0;i<n;i++){             int cnt=0;             for(int j=0;j<n;j++)                 {                     if(x[i]==x[j])                     cnt++;                 }             if(cnt==1)             {             System.out.print(x[i]+" ");             c++;             }
            }
            if((x.length-c)==x.length)
                    System.out.print(-1);
        }
    }

---
+++ Display_unique_values_in_an_Array.java
@@ -0,0 +1,31 @@
+
+import java.util.*;
+class sample{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n,x[],c=0;
+        n = sc.nextInt();
+        x = new int[n];
+        for(int i=0;i<n;i++){
+            x[i]= sc.nextInt();
+        }
+        for(int i=0;i<n;i++){
+            int cnt=0;
+            for(int j=0;j<n;j++)
+                {
+                    if(x[i]==x[j])
+                    cnt++;
+                }
+            if(cnt==1)
+            {
+            System.out.print(x[i]+" ");
+            c++;
+            }
+            
+            
+            
+        }
+        if((x.length-c)==x.length)
+                System.out.print(-1);
+    }
+}
