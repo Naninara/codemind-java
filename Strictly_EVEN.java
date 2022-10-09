b9dbc4862951763ab62a08bcea2dd1bfec1f541d
Naninara <106659791+Naninara@users.noreply.github.com>
2022-10-09 12:54:53 +0530

    import java.util.Scanner; class sample{     public static void main(String args[]){         Scanner sc = new Scanner(System.in);         int n,x[],cnt=0,cnt1=0;         n=sc.nextInt();         x= new int[n];         for(int i=0;i<n;i++){             x[i]=sc.nextInt();         }         for(int i=0;i<n;i++){             if(x[i]%2==0){                 cnt1++;             }         }         for(int i=0;i<n;i++){             if(x[i]%2==0 && i%2==0)             cnt++;         }         if(cnt1==cnt){             System.out.print("True");         }         else             System.out.print("False");     } }

---
+++ Strictly_EVEN.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+class sample{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n,x[],cnt=0,cnt1=0;
+        n=sc.nextInt();
+        x= new int[n];
+        for(int i=0;i<n;i++){
+            x[i]=sc.nextInt();
+        }
+        for(int i=0;i<n;i++){
+            if(x[i]%2==0){
+                cnt1++;
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(x[i]%2==0 && i%2==0)
+            cnt++;
+        }
+        if(cnt1==cnt){
+            System.out.print("True");
+        }
+        else
+            System.out.print("False");
+    }
+}
