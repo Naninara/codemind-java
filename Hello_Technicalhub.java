b8e30897ed042c4879bb7f97889b2dbab4470e0d
Naninara <106659791+Naninara@users.noreply.github.com>
2022-10-28 19:55:33 +0530

    import java.util.*; class reversestring { 	public static void main(String[] args) { 	Scanner sc= new Scanner(System.in); 	String s1 = sc.nextLine(); 	System.out.println("Hello Technicalhub"); 	System.out.println(s1);
    	sc.close();
    		
    
    	}
    
    }

---
+++ Hello_Technicalhub.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class reversestring {
+	public static void main(String[] args) {
+	Scanner sc= new Scanner(System.in);
+	String s1 = sc.nextLine();
+	System.out.println("Hello Technicalhub");
+	System.out.println(s1);
+	
+	sc.close();
+		
+
+	}
+
+}
