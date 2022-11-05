9f81764b0c9765c28ad43d30d7ba0bc0d9d14eb3
Naninara <106659791+Naninara@users.noreply.github.com>
2022-11-05 11:01:59 +0530

    //package numbers;
    import java.util.Scanner;
    
    class FirstAndLastPair {
    
    	public static void main(String[] args) {
    		Scanner sc = new Scanner(System.in);
    		int n= sc.nextInt();
    		int oddCount=0;
    		int arr[]= new int[n];
    		int a[]= new int[n];
    		int b[] = new int[n];
    		for(int i=0;i<n;i++) {
    			arr[i]=sc.nextInt();
    		}
    		int k=n/2;
    		int c=0;
    		int d=0;
    		for(int i=0;i<k;i++) {
    			a[c]=arr[i];
    			c++;
    		}
    		for(int i=n-1;i>=k;i--) {
    			b[d]=arr[i];
    			d++;
    		}
    		if(n%2==0) {
    			for(int i=0;i<c;i++) {
    				System.out.print(a[i]+" "+b[i]+" ");
    			}
    		}
    		else {
    			for(int i=0;i<c;i++) {
    				System.out.print(a[i]+" "+b[i]+" ");
    			}
    			System.out.print(b[k]);
    			System.out.print(" "+"0");
    		}
    
    	}
    
    }

--- First_and_Last_pair.java
+++ First_and_Last_pair.java
@@ -1,46 +1,45 @@
+//package numbers;
+
 import java.util.Scanner;
-class Wave
-{
-    public static void main(String args[])
-    {
-        int y=0,c=0,d=0;
-        int a[]=new int[20];
-        int b[]=new int[20];
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        int[] arr=new int[n];
-        for(int i=0;i<n;i++)
-        {
-            arr[i]=sc.nextInt();
-        }
-        int k=n/2;
-        for(int i=0;i<k;i++)
-        {
-            a[c]=arr[i];
-            c++;
-        }
-        for(int i=n-1;i>=k;i--)
-        {
-            b[d]=arr[i];
-            d++;
-        }
-        if(n%2==0)
-        {
-            for(int i=0;i<c;i++)
-            {
-                System.out.print(a[i]+" ");
-                System.out.print(b[i]+" ");
-            }
-        }
-        else
-        {
-            for(int i=0;i<c;i++)
-            {
-                System.out.print(a[i]+" ");
-                System.out.print(b[i]+" ");
-            }
-            System.out.print(b[k]+" ");
-            System.out.print("0");
-        }
-    }
+
+class FirstAndLastPair {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n= sc.nextInt();
+		int oddCount=0;
+		int arr[]= new int[n];
+		int a[]= new int[n];
+		int b[] = new int[n];
+		for(int i=0;i<n;i++) {
+			arr[i]=sc.nextInt();
+		}
+		int k=n/2;
+		int c=0;
+		int d=0;
+		for(int i=0;i<k;i++) {
+			a[c]=arr[i];
+			c++;
+		}
+		for(int i=n-1;i>=k;i--) {
+			b[d]=arr[i];
+			d++;
+		}
+		if(n%2==0) {
+			for(int i=0;i<c;i++) {
+				System.out.print(a[i]+" "+b[i]+" ");
+			}
+		}
+		else {
+			for(int i=0;i<c;i++) {
+				System.out.print(a[i]+" "+b[i]+" ");
+			}
+			System.out.print(b[k]);
+			System.out.print(" "+"0");
+		}
+
+	}
+
 }
+
+
