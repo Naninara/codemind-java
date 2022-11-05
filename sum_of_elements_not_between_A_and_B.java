c5d49482723ef0a26c3ff64fc12d2d3bde3bc025
Naninara <106659791+Naninara@users.noreply.github.com>
2022-11-05 09:52:47 +0530

    //package numbers; import java.util.*; class MinimumBwAandB {
    	public static void main(String[] args) {
    	Scanner sc = new Scanner(System.in);
    	int n= sc.nextInt();
    	int x[]= new int[n];
    	for(int i=0;i<n;i++) {
    		x[i]=sc.nextInt();
    	}
    	int a = sc.nextInt();
    	int b = sc.nextInt();
    	int c=0;
    	int y=0;
    	for(int i=0;i<n;i++)
    		if(x[i]<a || x[i]>b ) {
    			x[c]=x[i];
    			c++;
    			y=1;
    		}
    	    int cnt=0;
    		for(int i=0;i<c;i++) {
    			cnt =cnt+x[i];
    		}
    	System.out.print(cnt);
    	
    
    }
    }

--- sum_of_elements_not_between_A_and_B.java
+++ sum_of_elements_not_between_A_and_B.java
@@ -1,26 +1,30 @@
-import java.util.Scanner;
-class Minimum
-{
-    public static void main(String args[])
-    {
-        int n,count=0,countt=0,sum=0,y=0;
-        Scanner sc=new Scanner(System.in);
-        n=sc.nextInt();
-        int[] arr=new int[n];
-        for(int i=0;i<n;i++)
-        {
-            arr[i]=sc.nextInt();
-        }
-        int a=sc.nextInt();
-        int b=sc.nextInt();
-        for(int i=0;i<n;i++)
-        {
-            if(arr[i]<a || arr[i]>b)
-            {
-                sum=sum+arr[i];
-            }
-        }
-        System.out.print(sum);
-        sc.close();
-    }
+//package numbers;
+import java.util.*;
+class MinimumBwAandB {
+
+	public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	int n= sc.nextInt();
+	int x[]= new int[n];
+	for(int i=0;i<n;i++) {
+		x[i]=sc.nextInt();
+	}
+	int a = sc.nextInt();
+	int b = sc.nextInt();
+	int c=0;
+	int y=0;
+	for(int i=0;i<n;i++)
+		if(x[i]<a || x[i]>b ) {
+			x[c]=x[i];
+			c++;
+			y=1;
+		}
+	    int cnt=0;
+		for(int i=0;i<c;i++) {
+			cnt =cnt+x[i];
+		}
+	System.out.print(cnt);
+	
+
+}
 }
