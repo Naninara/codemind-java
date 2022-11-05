0ab3c7db09fadf0f78c2c4db52180a0537b111df
Naninara <106659791+Naninara@users.noreply.github.com>
2022-11-05 11:06:17 +0530

    //package numbers;
    import java.util.Scanner;
    
    class ElementsWithPairs {
    
    	public static void main(String[] args) {
    		// TODO Auto-generated method stub
    		Scanner sc = new Scanner(System.in);
    		int n= sc.nextInt();
    		int oddCount=0;
    		int x[]= new int[n];
    		for(int i=0;i<n;i++) {
    			x[i]=sc.nextInt();
    		}
    		if(n%2==0) {
    			for(int i=0;i<n;i++) {
    			System.out.print(x[i]+" ");
    		}
    		}
    		else {
    			for(int i=0;i<n;i++) {
    				System.out.print(x[i]+" ");
    		}
    			System.out.print("0");
    		}
    
    	}
    
    }

--- Elements_with_pairs.java
+++ Elements_with_pairs.java
@@ -1,25 +1,30 @@
+//package numbers;
+
 import java.util.Scanner;
-class Wave
-{
-    public static void main(String args[])
-    {
-        int y=0,c=0,d=0;
-        int a[]=new int[20];
-        int b[]=new int[20];
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        int[] arr=new int[n];
-        for(int i=0;i<n;i++)
-        {
-            arr[i]=sc.nextInt();
-        }
-        for(int i=0;i<n;i++)
-        {
-            System.out.print(arr[i]+" ");
-        }
-        if(n%2!=0)
-        {
-            System.out.print(0);
-        }
-    }
+
+class ElementsWithPairs {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n= sc.nextInt();
+		int oddCount=0;
+		int x[]= new int[n];
+		for(int i=0;i<n;i++) {
+			x[i]=sc.nextInt();
+		}
+		if(n%2==0) {
+			for(int i=0;i<n;i++) {
+			System.out.print(x[i]+" ");
+		}
+		}
+		else {
+			for(int i=0;i<n;i++) {
+				System.out.print(x[i]+" ");
+		}
+			System.out.print("0");
+		}
+
+	}
+
 }
