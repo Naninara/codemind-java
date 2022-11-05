1589ed494d88b0d4bf352c96eeef80a03fec5da0
Naninara <106659791+Naninara@users.noreply.github.com>
2022-11-05 10:21:41 +0530

    //package numbers;
    import java.util.Scanner;
    
    class ValueSameAsCount {
    
    	public static void main(String[] args) {
    		Scanner sc = new Scanner(System.in);
    		int n= sc.nextInt();
    		int x[]= new int[n];
    		for(int i=0;i<n;i++) {
    			x[i]=sc.nextInt();
    		}
    		int c=0;
    		int y=0;
    		for(int i=0;i<n;i++) {
    			if(x[i]==-1) {
    				continue;
    			}
    			int count=1;
    			for(int j=i+1;j<n;j++) {
    				if(x[i]==x[j]) {
    					x[j]=-1;
    					count++;
    				}
    			}
    			if(count==x[i]) {
    				x[c]=count;
    				c++;
    				y=1;
    			}
    			
    		}
    		
    			int cnt=0;
    			for(int i=0;i<c;i++) {
    				cnt++;
    			}
    		System.out.print(cnt);	
    		
    
    	}
    
    }

--- value_same_as_count.java
+++ value_same_as_count.java
@@ -1,41 +1,44 @@
-import java.util.*;
-class sample{
-    public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-        int n,x[],y[],k,flag=0;
-        n = sc.nextInt();
-        x= new int[n];
-        y= new int[n];
-        for(int i=0;i<n;i++){
-            x[i]=sc.nextInt();
-        }
-        
-        int visited=-1;
-        for(int i=0;i<n;i++){
-            int count=1;
-            for(int j=i+1;j<n;j++){
-                if(x[i]==x[j]){
-                    count++;
-                    y[j]=-1;
-                    
-                }
-            }
-            if(y[i]!=visited){
-                y[i]=count;
-                
-                
-            }
-        }
-        int c=0;
-        for(int i=0;i<n;i++){
-            if(x[i]==y[i]){
-                c++;
-            }
-        }
-        System.out.print(c);
-        // if(flag==0){
-        //     System.out.print("-1");
-        // }
-        
-    }
+//package numbers;
+
+import java.util.Scanner;
+
+class ValueSameAsCount {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n= sc.nextInt();
+		int x[]= new int[n];
+		for(int i=0;i<n;i++) {
+			x[i]=sc.nextInt();
+		}
+		int c=0;
+		int y=0;
+		for(int i=0;i<n;i++) {
+			if(x[i]==-1) {
+				continue;
+			}
+			int count=1;
+			for(int j=i+1;j<n;j++) {
+				if(x[i]==x[j]) {
+					x[j]=-1;
+					count++;
+				}
+			}
+			if(count==x[i]) {
+				x[c]=count;
+				c++;
+				y=1;
+			}
+			
+		}
+		
+			int cnt=0;
+			for(int i=0;i<c;i++) {
+				cnt++;
+			}
+		System.out.print(cnt);	
+		
+
+	}
+
 }
