ca7d5f5b788fe0ffb59c8fa9f87be833c8475dd0
Naninara <106659791+Naninara@users.noreply.github.com>
2022-12-16 20:42:25 +0530

    import java.util.*; class sample{     public static void main(String args[]){         Scanner sc = new Scanner(System.in);         int l = sc.nextInt();         int b = sc.nextInt();         int w = sc.nextInt();         int c = sc.nextInt();         int total = (l+2*w)*(b+2*w);         int park = (l*b);         System.out.print((total-park)*c);     } }

---
+++ Cost_to_fence_the_park_outside.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class sample{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int l = sc.nextInt();
+        int b = sc.nextInt();
+        int w = sc.nextInt();
+        int c = sc.nextInt();
+        int total = (l+2*w)*(b+2*w);
+        int park = (l*b);
+        System.out.print((total-park)*c);
+    }
+}
