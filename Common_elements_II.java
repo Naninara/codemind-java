356847f0a94a2a783ba04b9b6854527ba1bfc57a
Naninara <106659791+Naninara@users.noreply.github.com>
2022-12-16 19:51:01 +0530

    //package Problems;
    import java.util.ArrayList;
    import java.util.Scanner;
    
    class DisplayUncommon {
    
    	public static void main(String[] args) {
    		// TODO Auto-generated method stub
    		Scanner sc = new Scanner(System.in);
            int n = sc.nextInt();
            int m = sc.nextInt();
            ArrayList<Integer> li = new ArrayList<>();
            ArrayList<Integer> li1 = new ArrayList<>();
            
            for(int i=0;i<n;i++){
                li.add(sc.nextInt());
            }
            for(int i=0;i<m;i++){
                li1.add(sc.nextInt());
            }
            ArrayList<Integer> newl = new ArrayList<>();
            
            for(int ele:li) {
            	if(!li1.contains(ele)) {
            		newl.add(ele);
            	}
            }
            for(int ele:li1) {
            	if(!li.contains(ele)) {
            		newl.add(ele);
            	}
            }
            newl.forEach((i) -> System.out.print(i+" "));
            
            
            
    
    	}
    
    }

---
+++ Common_elements_II.java
@@ -0,0 +1,41 @@
+//package Problems;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class DisplayUncommon {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        ArrayList<Integer> li = new ArrayList<>();
+        ArrayList<Integer> li1 = new ArrayList<>();
+        
+        for(int i=0;i<n;i++){
+            li.add(sc.nextInt());
+        }
+        for(int i=0;i<m;i++){
+            li1.add(sc.nextInt());
+        }
+        ArrayList<Integer> newl = new ArrayList<>();
+        
+        for(int ele:li) {
+        	if(!li1.contains(ele)) {
+        		newl.add(ele);
+        	}
+        }
+        for(int ele:li1) {
+        	if(!li.contains(ele)) {
+        		newl.add(ele);
+        	}
+        }
+        newl.forEach((i) -> System.out.print(i+" "));
+        
+        
+        
+
+	}
+
+}
