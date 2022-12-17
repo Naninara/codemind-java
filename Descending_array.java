84ba3ad880a7cf4de2aa4ca4d68ebc675cb3d8bd
Naninara <106659791+Naninara@users.noreply.github.com>
2022-12-17 19:35:46 +0530

    import java.util.*; class AcendingArray {
        public static void main(String[] args) {
            Scanner sc = new Scanner(System.in);
            int x[],n;
            n = sc.nextInt();
    
            x =new int[n];
            for( int i=0;i<n;i++)
            {
                x[i]=sc.nextInt();
    
            }
    
            for( int i=0;i<n-1;i++)
            {
                if(x[i]<x[i+1])
                {
                    System.out.println("no");
                    System.exit(0);
                }
            }
            System.out.println("yes");
        }
    }

--- Descending_array.java
+++ Descending_array.java
@@ -1,26 +1,26 @@
 import java.util.*;
-class array3 {
-   
+class AcendingArray {
+
     public static void main(String[] args) {
-   Scanner sc = new Scanner(System.in); 
-   int x[],n,m,cnt1=0,cnt=0;
-   n = sc.nextInt();
+        Scanner sc = new Scanner(System.in);
+        int x[],n;
+        n = sc.nextInt();
 
-   x =new int[n];
-   for( int i=0;i<n;i++)
-   {
-    x[i]=sc.nextInt();
-
-   }
-   
-    for( int i=0;i<n-1;i++)
-    {
-        if(x[i]<x[i+1])
+        x =new int[n];
+        for( int i=0;i<n;i++)
         {
-            System.out.println("no");
-            System.exit(0);
+            x[i]=sc.nextInt();
+
         }
+
+        for( int i=0;i<n-1;i++)
+        {
+            if(x[i]<x[i+1])
+            {
+                System.out.println("no");
+                System.exit(0);
+            }
+        }
+        System.out.println("yes");
     }
-    System.out.println("yes");
-}
 }
